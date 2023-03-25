49deb32e2eb48564f0c20bffafe9d144aaa5f359
anubhav-18 <anubhavbindal18@gmail.com>
2023-03-25 23:04:34 +0530

    ui

--- Flutter/food_delivering_app/lib/main.dart
+++ Flutter/food_delivering_app/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: HomePage(),
       // themeMode: ThemeMode.light,
       // darkTheme: ThemeData(brightness: Brightness.dark),
--- Flutter/food_delivering_app/lib/pages/home_page.dart
+++ Flutter/food_delivering_app/lib/pages/home_page.dart
@@ -6,20 +6,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
+      drawer: const Drawer(),
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: const Center(
-          child: Text("Catalog", 
-            style: TextStyle(
-              color: Colors.white
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.yellow,
+        title: Text( "Grofers",
+              style: TextStyle(color: Colors.black , fontSize: 20)
+        ),
+      actions: [
+        CircleAvatar(
+          radius: 15,
+          backgroundColor: Color(0xffd4d181),
+          child: Icon(Icons.search,size: 17, color: Colors.black),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: Color(0xffd4d181),
+            child: Icon(Icons.shopping_cart,size:17,color: Colors.black,)
+          ),
+        )
+      ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Container(
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 126, 237, 130),
+                borderRadius: BorderRadius.circular(10)
+              ),
             ),
           )
-        ),
+        ],
       ),
-      body: const Center(
-        child: Text("Welocme to the App Made by Anubhav"),
-      ),
-      drawer: const Drawer(),
     );
   }
 }
