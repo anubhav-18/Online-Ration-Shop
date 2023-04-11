204fb66798c33183ee7d86af9b321e90bf37c4cf
anubhav-18 <anubhavbindal18@gmail.com>
2023-04-11 23:58:23 +0530

    Profile Done

--- Flutter/food_delivering_app/lib/pages/DrawerPage/MyHeaderDrawer.dart
+++ Flutter/food_delivering_app/lib/pages/DrawerPage/MyHeaderDrawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'My_Profile.dart';
+
 class MyHeaderDrawer extends StatelessWidget {
   const MyHeaderDrawer({
     super.key,
@@ -16,15 +18,60 @@ class MyHeaderDrawer extends StatelessWidget {
         height: 200,
         padding: EdgeInsets.only(top: 20),
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Container(
-            margin: EdgeInsets.only(top: 10, bottom: 10),
-            height: 70,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: NetworkImage(image),
-                  fit: BoxFit.scaleDown
-                )),
+          Stack(
+            children: [ 
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  // border: Border.all(
+                  //   width: 2,
+                  //   color: Colors.black
+                  // ),
+                  color: Colors.white ,
+                  // image: DecorationImage(
+                  //   image: NetworkImage(image),
+                  //   fit: BoxFit.cover
+                  // )
+                ),
+                child: Icon(
+                  Icons.person,
+                  size: 55,
+                  color: Color(0xff0378AD).withOpacity(0.5)),
+              ),
+              Positioned(
+                bottom: 12,
+                right: 0,
+                left: 45,
+                child: 
+                  InkWell(
+                    onTap: () {   
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => My_Profile())
+                      );
+                    },
+                    child: Container(
+                      height: 25,
+                      width: 25,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.black
+                        ),
+                        color: Colors.white
+                      ),
+                      child: Icon(
+                        Icons.edit,
+                        size: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                  )
+              )
+            ]
           ),
           Text(
             'Anubhav Bindal',
---
+++ Flutter/food_delivering_app/lib/pages/DrawerPage/My_Profile.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivering_app/pages/Home_Page.dart';
+
+class My_Profile extends StatefulWidget {
+  const My_Profile({super.key});
+
+  @override
+  State<My_Profile> createState() => _My_ProfileState();
+}
+
+class _My_ProfileState extends State<My_Profile> {
+  bool isObscurePassword = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Edit Profile'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 15, top: 20, right: 25),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              SizedBox(height: 30),
+              buildTextField("Full Name", "", false),
+              buildTextField("Email", "", false),
+              buildTextField("Phone No.", "", false),
+              buildTextField("DOB", "", false),
+              buildTextField("Password", "*******", true),
+              // buildTextField("Confirm Password","*******",true),
+              buildTextField("Address", "", false),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel",
+                        style: TextStyle(
+                            fontSize: 15,
+                            letterSpacing: 2,
+                            color: Colors.black)),
+                    style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                    },
+                    child: Text("Save",
+                        style: TextStyle(
+                            fontSize: 15,
+                            letterSpacing: 2,
+                            color: Colors.white)),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+        // obscureText: isPasswordTextField ? true : false,
+        obscureText: isObscurePassword,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+                    icon: Icon(Icons.remove_red_eye, color: Colors.grey),
+                    onPressed: () {
+                      setState(() {
+                        isObscurePassword = !isObscurePassword;
+                      });
+                    })
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 5),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            )),
+      ),
+    );
+  }
+}
