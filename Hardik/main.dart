58dd00caebb0c980ddcb9dc4907d7150ab6ff2c9
hnb2003 <128738211+hnb2003@users.noreply.github.com>
2023-04-11 23:43:35 +0530

    Add files via upload

---
+++ Hardik/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp(
+
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: EditProfile(),
+    );
+  }
+}
+
+class EditProfile extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  bool isObscurePassword = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        // actions: [
+        //   IconButton(
+        //       icon: const Icon
+        //         (
+        //         Icons.settings,
+        //         color: Colors.white,
+        //       ),
+        //       onPressed: () {}
+        //   )
+        // ],
+      ),
+      body: Container(
+        padding:  EdgeInsets.only(left: 15,top: 20,right: 25),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              // Center(
+                // child: Stack(
+                //   children: [
+                //     Container(
+                //       width: 130,
+                //       height: 130,
+                //       decoration: BoxDecoration(
+                //           border: Border.all(width: 4,color: Colors.yellowAccent),
+                //           boxShadow: [
+                //             BoxShadow(
+                //                 spreadRadius: 2,
+                //                 blurRadius: 10,
+                //                 color: Colors.black.withOpacity(0.1)
+                //             ),
+                //           ],
+                //           // shape: BoxShape.circle,
+                //           // image: const DecorationImage(
+                //           //     fit: BoxFit.cover,
+                //           //     image: AssetImage(
+                //           //         "Assets/imgs/grocerslogo.png"
+                //           //     )
+                //           // )
+                //       ),
+                //     ),
+                //     Positioned(
+                //         bottom: 0,
+                //         right: 0,
+                //
+                //         child: Container(
+                //           height: 40,
+                //           width: 40,
+                //           decoration: BoxDecoration(
+                //               shape: BoxShape.circle,
+                //               border: Border.all(
+                //                   width: 4,
+                //                   color: Colors.white
+                //               ),
+                //               color: Colors.yellow
+                //           ),
+                //           child: Icon(
+                //             Icons.edit,
+                //             color: Colors.white,
+                //           ),
+                //         )
+                //     )
+                //   ],
+                // ),
+              // ),
+              SizedBox(height: 30),
+              buildTextField("Full Name","",false),
+              buildTextField("Email","",false),
+              buildTextField("Phone No.","",false),
+              buildTextField("DOB","",false),
+              buildTextField("Password","*******",true),
+              // buildTextField("Confirm Password","*******",true),
+              buildTextField("Address","",false),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                      onPressed: () {},
+                      child: Text("Cancel",
+                        style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.black
+                        )),
+                      style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                      ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Save", style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.white
+                      )),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                    ),
+                  )
+                ],
+              )
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+  Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+        // obscureText: isPasswordTextField ? true : false,
+        obscureText: isObscurePassword,
+        decoration: InputDecoration(
+          suffixIcon: isPasswordTextField ?
+              IconButton(
+                icon: Icon(Icons.remove_red_eye, color: Colors.grey),
+                  onPressed: () {
+                    setState(() {
+                      isObscurePassword = !isObscurePassword;
+
+                      // obsc
+
+                    });
+                  }
+              ):null,
+          contentPadding: EdgeInsets.only(bottom: 5),
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          )
+
+        ),
+      ),
+    );
+  }
+}
