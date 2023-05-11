1d3dfa3191a9c357c9a19e623ba91fce4a58b8d2
Yash943 <yashdubey0204@gmail.com>
2023-05-11 17:41:40 +0530

    firebase hosting and connectivity

--- Flutter/food_delivering_app/lib/main.dart
+++ Flutter/food_delivering_app/lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivering_app/pages/DrawerPage/Categories.dart';
 import 'package:food_delivering_app/pages/DrawerPage/My_Order.dart';
 import 'package:food_delivering_app/pages/HomePage/Address.dart';
 import 'package:food_delivering_app/pages/Splash_Login/splash_screen.dart';
 import 'package:food_delivering_app/pages/cart/cartpage.dart';
+import 'firebase_options.dart';
 import 'pages/Home_Page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -15,14 +21,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CategoriesPage(),
+      home: SplashScreen(),
       initialRoute: '/',
       routes: {
-        '/home' :(context) => HomePage() ,
-        '/categories' :(context) => CategoriesPage(),
-        '/order' :(context) => My_Order(),
-        '/cart_page' :(context) => CartPage(),
-        '/address' :(context) => Address(),
+        '/home': (context) => HomePage(),
+        '/categories': (context) => CategoriesPage(),
+        '/order': (context) => My_Order(),
+        '/cart_page': (context) => CartPage(),
+        '/address': (context) => Address(),
       },
     );
   }
